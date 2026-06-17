287d7a3e52d00bb3bc9da797fe76fe3fd389420c
Vivian Pereira <vivian_pereira@outlook.com.br>
2026-06-16 23:41:01 -0300

    remove referência desnecessária ao tallstackui no layout HTML

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" x-data="tallstackui_darkTheme()" x-init="darkTheme = true">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" x-init="darkTheme = true">
 
     <head>
         <meta charset="utf-8">
